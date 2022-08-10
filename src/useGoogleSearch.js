64e0e35545d7239047aac8496dc2ba15f978b5e5
Joshua Siddle <106534417+Joshsiddle@users.noreply.github.com>
2022-08-10 23:08:14 +0100

    second commit

---
+++ src/useGoogleSearch.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+import API_KEY from './keys'
+
+const CONTEXT_KEY = "903c72868fc944769"
+
+const useGoogleSearch = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+
+    }, [term])
+
+}
+
+export default useGoogleSearch
